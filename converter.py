3d1fbd3bf9831d3aeaf8245b70ceaae7e3802008
ScriptLineStudios <scriptlinestudios@protonmail.com>
2023-11-05 13:09:55 +0200

    changes

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
 text = """
-#version 330
+#version 330 core
 layout(location=0) in vec2 aPos;
 layout(location=1) in vec3 aCol;
 
@@ -12,7 +12,7 @@ void main() {
 """
 
 text2 = """
-#version 330
+#version 330 core
 in vec3 color;
 
 void main() {
@@ -20,6 +20,6 @@ void main() {
 }
 """
 
-for word in text:
+for word in text2:
     print(hex(ord(word)), end=", ")
 print(0)
--- main.c
+++ main.c
@@ -1,21 +1,16 @@
 #include <stddef.h>
 #include <stdio.h>
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+static float beans[4] = {1.0, 1.0, 1.0, 1.0};
+
+int bruh(float *yes) {
+
+}
+
 int main(void) {
-    // glfwInit();
-
-    // GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-    // printf("%ld\n", sizeof(window));
-
-    // glfwTerminate();
-    printf("%ld\n", sizeof(float));
-    printf("%ld\n", sizeof(double));
-
-    double d = 0.12309489873923849;
-    printf("%.100lf\n", d);
-    printf("%.100f\n", d);
-
+    bruh(beans);
     return 0;
 }
