09c74f56dd814730d118b6aeda91ad478d859841
ScriptLineStudios <scriptlinestudios@protonmail.com>
2023-11-05 22:29:06 +0200

    wow a triangle

--- main.asm
+++ main.asm
@@ -5,23 +5,16 @@ extern glad_glClear, glad_glClearColor, glad_glGenVertexArrays, glad_glBindVerte
 extern glad_glVertexAttribPointer, glad_glEnableVertexAttribArray
 extern glad_glCreateShader, glad_glShaderSource, glad_glCompileShader
 extern glad_glCreateProgram, glad_glAttachShader, glad_glLinkProgram, glad_glUseProgram
-extern glad_glDrawArrays
+extern glad_glDrawArrays, glad_glGetShaderiv
 extern glfwInit, glfwTerminate, glfwCreateWindow, glfwSwapBuffers, glfwPollEvents, glfwWindowShouldClose, gladLoadGLLoader, glfwGetProcAddress, glfwMakeContextCurrent
 
-extern printf
+extern printf, exit
 
-print:
-    push rbp
-    mov rsp, rbp
-
-    mov rdx, len
-    mov rsi, vsrc
-    mov rax, 1
-    mov rdi, 1
-    syscall
-
-    pop rbp
-    ret
+%define GL_ARRAY_BUFFER 34962
+%define GL_STATIC_DRAW 35044
+%define GL_DOUBLE 5130
+%define GL_VERTEX_SHADER 35633
+%define GL_FRAGMENT_SHADER 35632
 
 main:
     push rbp
@@ -39,128 +32,124 @@ main:
     mov rdi, [window]
     call glfwMakeContextCurrent
 
-
     ; Load gl pointers using glad
     mov rdi, glfwGetProcAddress
     call gladLoadGLLoader
     
     cmp rax, 0
     jne glgarbage
-    call print
     jmp term
     ; Finished with glad
 
 glgarbage:
-    mov rdi, string_format
-    mov rsi, vsrc
-    call printf
-
-    ; VAO and VBO
+    ;%rdi, %rsi, %rdx, %rcx, %r8, and %r9
     mov rdi, 1
     mov rsi, vao
     call [glad_glGenVertexArrays]
     mov rdi, [vao]
     call [glad_glBindVertexArray]
-    
-    mov rdi, int_format
-    mov rsi, [vao]
-    call printf
-    ; VAO complete
 
-    mov rdi, float_format
-    movq xmm0, [vertices + 8*(5)]
-    mov rax, 1
-    call printf
+    ; glGenBuffers(1, &vbo);
+    mov rdi, 1
+    mov rsi, vbo
+    call [glad_glGenBuffers]
 
-    ; attrb pointers
+    ;glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    mov rdi, GL_ARRAY_BUFFER
+    mov rsi, [vbo]
+    call [glad_glBindBuffer]
+
+    ; glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    mov rdi, GL_ARRAY_BUFFER
+    mov rsi, 120
+    mov rdx, vertices
+    mov rcx, GL_STATIC_DRAW
+    call [glad_glBufferData]
+
+    ; glVertexAttribPointer(0, 2, GL_DOUBLE, GL_FALSE, sizeof(double) * 5, (void *)0);
     mov rdi, 0
     mov rsi, 2
-    mov rdx, 0x140a
+    mov rdx, GL_DOUBLE
     mov rcx, 0
     mov r8, 40
     mov r9, 0
     call [glad_glVertexAttribPointer]
 
+    ; glEnableVertexAttribArray(0);
     mov rdi, 0
     call [glad_glEnableVertexAttribArray]
 
+    ; glVertexAttribPointer(1, 3, GL_DOUBLE, GL_FALSE, sizeof(double) * 5, (void *)(2 * sizeof(double)));
     mov rdi, 1
     mov rsi, 3
-    mov rdx, 0x140a
+    mov rdx, GL_DOUBLE
     mov rcx, 0
-    mov r8, 40 
-    mov r9, 16 ; sizeof(double) * 2
+    mov r8, 40
+    mov r9, 16
     call [glad_glVertexAttribPointer]
 
+    ; glEnableVertexAttribArray(1);
     mov rdi, 1
     call [glad_glEnableVertexAttribArray]
-    ; attrib pointers complete
 
-    mov rdi, 1
-    mov rsi, vbo
-    call [glad_glGenBuffers]
-    mov rdi, 34962
-    mov rsi, [vbo]
-    call [glad_glBindBuffer]
-    mov rdi, 34962
-    mov rsi, 120
-    mov rdx, vertices
-    mov rcx, 0x8892
-    call [glad_glBufferData]
+    ; VERTEX 
 
-    ; ------------------------ Its shadering time --------------------------;
-    mov rdi, 0x8B31
+    ; vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    mov rdi, GL_VERTEX_SHADER
     call [glad_glCreateShader]
-    mov [vshader], rax
+    mov [vshader], rax ; move rax into vshader
+
+    ; glShaderSource(vertex_shader, 1, &vertex_src, NULL);
     mov rdi, [vshader]
     mov rsi, 1
     mov rdx, pvsrc
     mov rcx, 0
     call [glad_glShaderSource]
+
+    ; glCompileShader
     mov rdi, [vshader]
     call [glad_glCompileShader]
 
-    mov rdi, 0x8B30
+    ; FRAGMENT
+
+    ; fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    mov rdi, GL_FRAGMENT_SHADER
     call [glad_glCreateShader]
-    mov [fshader], rax
+    mov [fshader], rax ; move rax into vshader
+
+    ; glShaderSource(fragment_shader, 1, &fragment_src, NULL);
     mov rdi, [fshader]
     mov rsi, 1
     mov rdx, pfsrc
     mov rcx, 0
     call [glad_glShaderSource]
+
+    ; glCompileShader
     mov rdi, [fshader]
     call [glad_glCompileShader]
 
-    mov rdi, int_format
-    mov rsi, [vshader]
-    call printf
-
-    mov rdi, int_format
-    mov rsi, [fshader]
-    call printf
-
-    ; ------------------------- Shaders DONE ---------------------------------;
-
-
-    ; ------------------------- Shader program ------------------------------;
+    ; program = glCreateProgram();
     call [glad_glCreateProgram]
     mov [program], rax
+
+    ;glAttachShader(program, vertex_shader);
     mov rdi, [program]
     mov rsi, [vshader]
     call [glad_glAttachShader]
+
+    ;glAttachShader(program, fragment_shader);
     mov rdi, [program]
     mov rsi, [fshader]
     call [glad_glAttachShader]
+
+    ; glLinkProgram(program);
     mov rdi, [program]
     call [glad_glLinkProgram]
+
+    ; glUseProgram(program);
     mov rdi, [program]
     call [glad_glUseProgram]
-    ; ------------------------- Shader program DONE ---------------------------;
 
-    mov rdi, int_format
-    mov rsi, [program]
-    call printf
-    
 loop:
     movd xmm0, [zero]
     movd xmm1, [r]
@@ -170,9 +159,6 @@ loop:
     
     mov rdi, 16384
     call [glad_glClear]
-    
-    mov rdi, [vao]
-    call [glad_glBindVertexArray]
 
     mov rdi, [program]
     call [glad_glUseProgram]
@@ -204,9 +190,7 @@ section .bss
     vshader: resb 4
     fshader: resb 4
     program: resb 4
-
-verts:
-    dd 1.0
+    status: resb 4
 
 section .rodata
     int_format: db "%d", 10, 0
@@ -216,10 +200,10 @@ section .rodata
     title: db "Hello, world!", 0
     error: db "Failed to load gl functions!", 10
     len equ $ - error
-    r: dd 0.2
+    r: dd 0.1
     test: dq 0.2
-    zero: dd 0.0
-    vertices: dq -0.5, -0.5, 1.0, 0.0, 0.0, 0.0, 0.37, 0.0, 1.0, 0.0, 0.5, -0.5, 0.0, 0.0, 1.0     
+    zero: dd 0.1
+    vertices: dq -0.5, -0.5, 1.0, 0.0, 0.0,   0.0, 0.37, 0.0, 1.0, 0.0,    0.5, -0.5, 0.0, 0.0, 1.0      
     vsrc:
         db 0xa, 0x23, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x20, 0x33, 0x33, 0x30, 0x20, 0x63, 0x6f, 0x72, 0x65, 0xa, 0x6c, 0x61, 0x79, 0x6f, 0x75, 0x74, 0x28, 0x6c, 0x6f, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x3d, 0x30, 0x29, 0x20, 0x69, 0x6e, 0x20, 0x76, 0x65, 0x63, 0x32, 0x20, 0x61, 0x50, 0x6f, 0x73, 0x3b, 0xa, 0x6c, 0x61, 0x79, 0x6f, 0x75, 0x74, 0x28, 0x6c, 0x6f, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x3d, 0x31, 0x29, 0x20, 0x69, 0x6e, 0x20, 0x76, 0x65, 0x63, 0x33, 0x20, 0x61, 0x43, 0x6f, 0x6c, 0x3b, 0xa, 0xa, 0x6f, 0x75, 0x74, 0x20, 0x76, 0x65, 0x63, 0x33, 0x20, 0x63, 0x6f, 0x6c, 0x6f, 0x72, 0x3b, 0xa, 0xa, 0x76, 0x6f, 0x69, 0x64, 0x20, 0x6d, 0x61, 0x69, 0x6e, 0x28, 0x29, 0x20, 0x7b, 0xa, 0x20, 0x20, 0x20, 0x20, 0x67, 0x6c, 0x5f, 0x50, 0x6f, 0x73, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x20, 0x3d, 0x20, 0x76, 0x65, 0x63, 0x34, 0x28, 0x61, 0x50, 0x6f, 0x73, 0x2e, 0x78, 0x79, 0x2c, 0x20, 0x30, 0x2e, 0x30, 0x2c, 0x20, 0x31, 0x2e, 0x30, 0x29, 0x3b, 0x20, 0xa, 0x20, 0x20, 0x20, 0x20, 0x63, 0x6f, 0x6c, 0x6f, 0x72, 0x20, 0x3d, 0x20, 0x61, 0x43, 0x6f, 0x6c, 0x3b, 0xa, 0x7d, 0xa, 0
     fsrc:
